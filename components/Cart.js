23a44ae990878b4bcb1af02c2e532f299ab372b5
Marko Siilak <marko@siilak.com>
2023-01-07 03:24:50 +0200

    Dev update

--- components/Cart.js
+++ components/Cart.js
@@ -1,69 +1,73 @@
-// Libs
-import { prestaEvents } from '../lib/api/api';
-import { getProduct } from '../lib/api/product';
-import { getLanguageByIsoCode } from '../lib/api/languages';
+import { Box } from "@mui/material";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
-// Core
-import { useState, useEffect } from 'react';
-import { useRouter } from 'next/router'
-
+import { prestaEvents } from "../lib/api/api";
+import { getLanguageByIsoCode } from "../lib/api/languages";
+import { getProduct } from "../lib/api/product";
 
 export default function Cart() {
+  const [cart, setCart] = useState(0);
+  const [products, setProducts] = useState([]);
 
-    const [cart, setCart] = useState(0);
-    const [products, setProducts] = useState([]);
+  const { locale } = useRouter();
 
-    const { locale } = useRouter();
+  useEffect(() => {
+    prestaEvents.on("updateCart", async function(cart) {
+      setCart(cart);
 
-    useEffect(() => {
-        prestaEvents.on('updateCart', async function(cart){
-            setCart(cart);
+      let cartProductsInfo = new Array();
+      const language = await getLanguageByIsoCode(locale);
 
-            let cartProductsInfo = new Array();
-            const language = await getLanguageByIsoCode(locale);
+      if (cart?.associations?.cart_rows && cart.associations.cart_rows.length > 0) {
+        cartProductsInfo = await Promise.all(
+          cart?.associations?.cart_rows.map(async element => {
+            const product = await getProduct(
+              element.id_product,
+              element.id_product_attribute,
+              language.id
+            );
+            product.quantity = element.quantity;
+            return product;
+          })
+        );
+      } else {
+        cartProductsInfo = [];
+      }
 
-            if(cart?.associations?.cart_rows && cart.associations.cart_rows.length > 0) {
-                cartProductsInfo = await Promise.all(
-                    cart?.associations?.cart_rows.map(
-                        async (element) => {
-                            let product = await getProduct(element.id_product, element.id_product_attribute, language.id);
-                            product.quantity = element.quantity;
-                            return product;
-                        }
-                    )
-                );
-            } else {
-                cartProductsInfo = [];
-            }
+      setProducts(cartProductsInfo);
+    });
+  }, []);
 
-            setProducts(cartProductsInfo);
-        });
-    }, []);
+  const reducerPrice = (accumulator, currentProduct) =>
+    parseFloat(accumulator) +
+    parseFloat(currentProduct.price_wt).toFixed(2) * currentProduct.quantity;
+  const reducerQuantity = (accumulator, currentProduct) =>
+    accumulator + parseInt(currentProduct.quantity);
 
-    const reducerPrice = (accumulator, currentProduct) => parseFloat(accumulator) + (parseFloat(currentProduct.price_wt).toFixed(2) * currentProduct.quantity);
-    const reducerQuantity = (accumulator, currentProduct) => accumulator + parseInt(currentProduct.quantity);
+  return (
+    <Box>
+      <p>Cart</p>
+      <p>You have {products?.reduce(reducerQuantity, 0) || 0} items on your cart.</p>
 
-    return (
-        <>
-            <p>Cart</p>
-            <p>You have {products?.reduce(reducerQuantity, 0) || 0} items on your cart.</p>
+      {products.map(product => {
+        return (
+          <p key={product.id}>
+            {product.quantity}x {product.name} - {parseFloat(product.price_wt).toFixed(2)}
+          </p>
+        );
+      })}
 
-            {products.map(
-                (product) => {
-                return (<p key={product.id}>{product.quantity}x {product.name} - {parseFloat(product.price_wt).toFixed(2)}</p>)
-                }
-            )}
-
-            <p>Total: {parseFloat(products?.reduce(reducerPrice, 0)).toFixed(2)}</p>
-        </>
-    )
+      <p>Total: {parseFloat(products?.reduce(reducerPrice, 0)).toFixed(2)}</p>
+    </Box>
+  );
 }
 
 export async function getStaticProps() {
-    return {
-        props : {
-            cart : null,
-            products : []
-        }
+  return {
+    props: {
+      cart: null,
+      products: []
     }
+  };
 }
--- pages/[category].js
+++ pages/[category].js
@@ -1,82 +1,74 @@
 // React
-import { useRouter } from 'next/router'
-import { useEffect } from 'react';
-
-// Libs
-import { getCategories, getCategory, getCategoryByURL } from '../lib/api/category';
-import { getCategoryProducts } from '../lib/api/product';
-import { initSession } from '../lib/api/api';
-import { getLanguages } from '../lib/api/languages';
-
-// Components
-import ProductCard from '../components/ProductCard';
-import Cart from '../components/Cart';
-
 // Packages
-import parse from 'html-react-parser';
+import parse from "html-react-parser";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+import Cart from "../components/Cart";
+// Components
+import ProductCard from "../components/ProductCard";
+import { initSession } from "../lib/api/api";
+// Libs
+import { getCategories, getCategory, getCategoryByURL } from "../lib/api/category";
+import { getLanguages } from "../lib/api/languages";
+import { getCategoryProducts } from "../lib/api/product";
 
 const Category = ({ categoryData, products }) => {
-    useEffect(() => {
-        initSession();
-    });
+  useEffect(() => {
+    initSession();
+  });
 
-    return (
-        <>
-            <Cart />
-            <p>{categoryData.name}</p>
-            {parse(categoryData.description)}
+  return (
+    <>
+      <Cart />
+      <p>{categoryData.name}</p>
+      {parse(categoryData.description)}
 
-            <section className="products">
-                {products?.map(
-                    (product, index) => {
-                        return (
-                            <ProductCard
-                                key={index}
-                                name={product.name}
-                                id={product.id}
-                                price={product.price_wt} />
-                        )
-                    }
-                )}
-            </section>
-        </>
-    );
-}
+      <section className="products">
+        {products?.map((product, index) => {
+          return (
+            <ProductCard key={index} name={product.name} id={product.id} price={product.price_wt} />
+          );
+        })}
+      </section>
+    </>
+  );
+};
 
 export default Category;
 
-export async function getStaticProps( { params, locale } ) {
-    const { category } = params;
+export async function getStaticProps({ params, locale }) {
+  const { category } = params;
 
-    const categoryData = await getCategoryByURL(category, locale);
-    const products = await getCategoryProducts(categoryData.id, locale);
+  const categoryData = await getCategoryByURL(category, locale);
+  const products = await getCategoryProducts(categoryData.id, locale);
 
-    return {
-        props : {
-            categoryData,
-            products
-        },
-        revalidate: 1
-    };
+  return {
+    props: {
+      categoryData,
+      products
+    },
+    revalidate: 1
+  };
 }
 
 export async function getStaticPaths() {
-    const { categories } = await getCategories();
-    const languages = await getLanguages();
+  const { categories } = await getCategories();
+  const languages = await getLanguages();
 
-    let paths = [];
+  const paths = [];
 
-    languages.map((lang, indexLang) => {
-        for(const categoryData in categories) {
-            paths.push({
-                params: { category: categories[categoryData].link_rewrite[indexLang].value },
-                locale : lang.iso_code
-            });
-        }
-    });
+  languages.map((lang, indexLang) => {
+    for (const categoryData in categories) {
+      paths.push({
+        params: { category: categories[categoryData].link_rewrite[indexLang].value },
+        locale: lang.iso_code
+      });
+    }
+  });
 
-    return {
-        paths,
-        fallback: false
-    };
+  return {
+    paths,
+    fallback: false
+  };
 }
